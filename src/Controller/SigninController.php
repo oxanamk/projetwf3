c0fa47c11a7ac612d160c7cb8fd4987e2de5de6d
oxanamk <oxanamk@outlook.fr>
2021-03-30 16:14:11 +0200

    header+1ere partie signin

---
+++ src/Controller/SigninController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Users;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SigninController extends AbstractController
+{
+    /**
+     * @Route("/signin", name="signin")
+     */
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        if ($this->getUser()) {
+            return $this->redirectToRoute('accueil');
+        }
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('signin/index.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+    }
+}
